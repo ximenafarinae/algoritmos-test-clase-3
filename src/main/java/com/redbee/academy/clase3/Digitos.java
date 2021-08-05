a522a8c5606ce9c4047f57bb1659fe876b67d3b4
Diego Navarrete <diego.navarrete92@gmail.com>
2021-08-05 11:48:09 -0300

    fix

--- src/main/java/com/redbee/academy/clase3/Digitos.java
+++ src/main/java/com/redbee/academy/clase3/Digitos.java
@@ -10,23 +10,6 @@ public class Digitos {
      */
     public static Integer resolver(Long numero) {
         //TODO: implementar
-
-        Long divisorInicial = 1L;
-        Boolean yaDioCero = false;
-        Integer cantidadNumeros = 0;
-
-        while (!yaDioCero) {
-            if(numero / divisorInicial > 0) {
-                cantidadNumeros++;
-                divisorInicial *= 10;
-            } else if(numero.equals(0L)) {
-                cantidadNumeros = 1;
-                yaDioCero = true;
-            } else {
-                yaDioCero = true;
-            }
-        }
-
-        return cantidadNumeros;
+        return null;
     }
 }
